f5cc7147085d0237ed66ed7bdcb8368c5c3ccff8
mawejjejohnken256 <mawejjeken884@gmail.com>
2024-10-04 02:18:11 -0700

    Add files via upload

---
+++ java loops.java
@@ -0,0 +1,23 @@
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		
+	for(int i =0; i<10; i++) {
+		System.out.println("I love Java");{
+	}
+		
+	}
+	int a =0;
+	while(a< 10) {
+		System.out.println("a is less than 10;" + a);
+	}
+	}
+	int a = 0;
+	{
+		System.out.println("a");
+	}
+	
+
+}
+     
